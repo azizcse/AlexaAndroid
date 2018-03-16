500d58a2de181b3564c7b6a406962dceb7fd192b
Faisal <faisal@w3engineers.com>
2018-03-16 15:02:53 +0600

    Added and modify files for UI

--- app/src/main/java/com/willblaschko/android/avs/LoginActivity.java
+++ app/src/main/java/com/willblaschko/android/avs/LoginActivity.java
@@ -2,13 +2,14 @@ package com.willblaschko.android.avs;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /*
 *  ****************************************************************************
-*  * Created by : Ahmed Mohmmad Ullah (Azim) on 3/16/2018 at 10:24 AM.
+*  * Created by : Ahmed Mohmmad Ullah (Azim) on 3/16/2018 at 11:26 AM.
 *  * Email : azim@w3engineers.com
 *  * 
 *  * Last edited by : Ahmed Mohmmad Ullah (Azim) on 3/16/2018.
@@ -16,24 +17,26 @@ import android.support.v7.app.AppCompatActivity;
 *  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>  
 *  ****************************************************************************
 */
-public class SplashActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 2000;
+    private Button button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+
+        Button btnSave = (Button) findViewById(R.id.button_save_user_name);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(i);
-                overridePendingTransition(R.anim.right_to_left, R.anim.left_to_right);
                 finish();
             }
-        }, SPLASH_TIME_OUT);
+        });
+
     }
 }
